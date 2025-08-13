89bddaa6785b3bd1bb83cf707237fe7235c2884b
danii <dynn5723@gmail.com>
2025-08-13 10:47:16 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+name = input()
+
+print("\t\t\t\t\t*\n\n" , "\t\t\t\t *\t\t*\n\n\n" , "\t\t\t*\t\t\t\t*\n\n\n\n" , "\t\t*                " , "Hi, " , name , "\t\t\t*\n\n\n" ,  "\t\t\t*\t\t\t\t*\n\n\n\n" , "\t\t\t\t*\t\t*\n\n\n" , "\t\t\t\t\t*\n\n")
