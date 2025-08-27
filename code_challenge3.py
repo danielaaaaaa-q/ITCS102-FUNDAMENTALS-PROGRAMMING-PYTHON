bd0c6f8ce052648bb4f2d74ed255a5291caba962
danii <dynn5723@gmail.com>
2025-08-27 11:08:51 +0800

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,11 +1,14 @@
+from getpass import getpass
+
 username = "danielaaaaaa-q"
 password = "dani13"
 
 user_name = input("Enter your username: ")
-isPassword = input("Enter your password: ")
+isPassword = getpass("Enter your password: ")
 
 
 if username == user_name and password == isPassword:
 	print("Access granted.")
 else:
+
 	print("Access denied.")
