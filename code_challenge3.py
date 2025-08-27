0b2537c3fb403059372b36ab26cae8f8c05b5caa
danii <dynn5723@gmail.com>
2025-08-27 11:07:36 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,11 @@
+username = "danielaaaaaa-q"
+password = "dani13"
+
+user_name = input("Enter your username: ")
+isPassword = input("Enter your password: ")
+
+
+if username == user_name and password == isPassword:
+	print("Access granted.")
+else:
+	print("Access denied.")
