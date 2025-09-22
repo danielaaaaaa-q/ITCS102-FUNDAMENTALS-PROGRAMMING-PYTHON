6ca3fcbd446f3487e791cb219071f76dd24a8836
dynneal <dynn5723@gmail.com>
2025-09-22 16:57:21 +0800

    Add files via upload

---
+++ code_challenge11.py
@@ -0,0 +1,24 @@
+for d in range(1, 11, 1):
+    for y in range(10, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("*", end=" ")
+    for m in range(1, d+1, 1):
+        print ("*", end=" ")
+    print()
+
+for d in range(9, 0, -1):
+    for y in range(10, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("*", end=" ")
+    for m in range(1, d+1, 1):
+        print ("*", end=" ")
+    print()
+
+
+
+    
+
+
+
