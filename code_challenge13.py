c84249aa52a17b5c35a3bc8fccc8c0d233a991b3
dynneal <dynn5723@gmail.com>
2025-10-15 14:20:05 +0800

    Add files via upload

---
+++ code_challenge13.py
@@ -0,0 +1,49 @@
+for d in range(1, 5, 1):
+    for y in range(25, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("⭐️", end="")
+    for m in range(1, d+1, 1):
+        print ("⭐️", end="")
+    print()
+for d in range(3, 0, -1):
+    for y in range(25, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("⭐️", end="")
+    for m in range(1, d+1, 1):
+        print ("⭐️", end="")
+    print()
+    
+for d in range(1, 8, 1):
+    for y in range(25, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("🌲", end="")
+    for m in range(1, d+1, 1):
+        print ("🌲", end="")
+    print()
+
+for d in range(1, 12, 1):
+    for y in range(25, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("🌲", end="")
+    for m in range(1, d+1, 1):
+        print ("🌲", end="")
+    print()
+
+for d in range(1, 15, 1):
+    for y in range(25, d, -1):
+        print(" ", end=" ")
+    for n in range(1, d, 1):
+        print("🌲", end="")
+    for m in range(1, d+1, 1):
+        print ("🌲", end="")
+    print()
+
+for d in range(1, 7, 1):
+    for y in range(21, 1, -1):
+        print(" ", end=" ")
+    print("🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵🪵", end="     ")
+    print()
