b6d28fdddfe917ea7d9edc5d99fb838567ceb5ac
dynneal <dynn5723@gmail.com>
2025-11-27 12:24:43 +0800

    Update finals_project.py

--- finals_project.py
+++ finals_project.py
@@ -1,3 +1,4 @@
+import os
 from act1_9 import *
 from if_else import *
 
@@ -9,20 +10,25 @@ con = True
 list_projects = []
 
 name = input("Please enter your name: ")
+
+os.system('cls')
+
 print(f"\nHello, {name}! Welcome to my compiler.")
 print("--> Here you can explore the topics and activities I have learned and done throughout the semester.")
+
 
 while con == True:
     print("\nSelect a topic below: \n1. Variables and Data Types\n2. Functions and String\n3. Operators\n4. If-Elif-Else Statements\n5. Loops\n6. Lists\n7. Code Challenges\n8. Exit Compiler")
     user_input = input("Enter your choice (1-7): ")
 
     if user_input == "1":
+        os.system('cls')
         print("``````````````````````````````````````````")
         print("--> Selected Topic: Variables and Data Types")
         print("Available Activities: \na. Activity 2")
         activity = input("Select an activity (a): ")
         if activity.lower() == "a":
-            act2()
+            name()
             continue
         else:
             print("Invalid activity selection.")
@@ -34,13 +40,13 @@ while con == True:
         print("Available Activities: \na. Activity 1\nb. Activity 3\nc. Activity 4\nd. Activity 5\ne. Activity 15")
         activity1 = input("Select an activity (a-e): ")
         if activity1.lower() == 'a':
-            act1()
+            hello_world()
             continue
         elif activity1.lower() == 'b':
-            act3()
+            escape_seq()
             continue
         elif activity1.lower() == 'c':
-            act4()
+            length()
             continue
         elif activity1.lower() == 'd':
             act5()
@@ -51,6 +57,9 @@ while con == True:
         else:
             print("Invalid activity selection.")
             continue
+
+        os.system('cls')
+
     elif user_input == "3":
         print("``````````````````````````````````````````")
         print("--> Selected Topic: Operators")
@@ -69,7 +78,29 @@ while con == True:
             print("Invalid activity selection.")
             continue
         continue
+
+        os.system('cls')
+
     elif user_input == "4":
         print("``````````````````````````````````````````")
         print("--> Selected Topic: If-Elif-Else Statements")
-        
+        continue
+
+    elif user_input == "5":
+        print("``````````````````````````````````````````")
+        print("--> Selected Topic: Loops")
+        continue
+
+    elif user_input == "6":
+        pass
+        continue
+
+    elif user_input == "7":
+        pass 
+        continue
+
+    elif user_input == "8":
+        print("Exiting Compiler...")
+
+    else: 
+        print("Invalid input, please try again.")
