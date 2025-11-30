ce40ee20f3cace3d182bb57faf28b6faa2d31d1d
dynneal <dynn5723@gmail.com>
2025-11-30 14:16:13 +0800

    ..

--- finals_project.py
+++ finals_project.py
@@ -12,7 +12,7 @@ def printing_opt():
 def print_menu():
     print("\n\t----------------------------------------------------")
     print("\t\t\tMenu to Printing in Python")
-    print("\t\ta. Simple Printing\n\t\tb. Print with numbers\n\t\tc. Printing with string and concatenation\n\t\td. back to main menu")
+    print("\t\ta. Simple Printing\n\t\tb. Print with numbers\n\t\tc. Printing with string and concatenation\n\t\td. Escape Sequence\n\t\te. Back to Main Menu")
     print("\t----------------------------------------------------")
 
 def print_output():
@@ -29,8 +29,8 @@ def print_input():
 
 def print_numbers():
     print('''
-    
     *We can also display numbers in print() function. However, we do not put it in double quotes.
+          
     ·INPUT:
 
     print(3)
@@ -51,7 +51,7 @@ def Var():
 
 def print_cont():
     print('''
-    1. Using a "+" operator
+    1. Using a "+" operator (concatenation)
 
     #to use this, we should put some variables first
     name = "John"
@@ -70,4 +70,73 @@ def print_cont():
 
     - Output:
     I live in Lucena City since 2015
+          
+    3. Formatted String (f-strings)
+          
+    country = "Philippines"
+    province = "Quezon Province"
+    print(f"I am from {province}, {country}")
+          
+    - Output:
+    I am from Quezon Province, Philippines
     ''')
+
+def escape_seq():
+    print('''
+    Escape sequences are special characters that are used to represent certain whitespace or non-printable characters in a string. 
+    Some common escape sequences in Python include:
+
+    \\n : Newline
+    \\t : Tab
+    \\\\ : Backslash
+    \\' : Single Quote
+    \\" : Double Quote
+
+    Example:
+    print("Hello, World!\\nWelcome to Python programming.")
+          
+    - Output:
+    Hello, World!
+    Welcome to Python programming.
+    ''')
+
+def conditional_statements():
+    print('''
+    Python conditional statements are commands that let a program decide what action to take based on a condition. They check whether something is true or false, and then run specific code depending on the result.
+    ''')
+
+def conditional_menu():
+    print("\n\t----------------------------------------------------")
+    print("\t\t\tMenu to Conditional Statements")
+    print("\t\ta. If Statements\n\t\tb. If-Else Statements\n\t\tc. Elif Statements\n\t\td. Nested If Statements\n\t\te. Back to Main Menu")
+    print("\t----------------------------------------------------") 
+
+def if_statements():
+    print('''
+    An if statement is a conditional statement that executes a block of code if a specified condition is true.
+          
+    == - equals
+    != - not equals
+    >  - greater than
+    <  - less than
+    >= - greater than or equal to
+    <= - less than or equal to
+
+    Syntax:
+    if condition:
+        # code to be executed if condition is true  
+          
+    Example: #jeepney fare
+
+    name = input("Input your name: ")
+    isStudent = input("Are you a student (yes/no): ")
+    fare = eval(input("bayad: "))
+
+    if isStudent.lower() == "yes":
+	    discount = fare * .2
+	    new_fare = fare - discount
+	    print("Hi,", name, "your student discount is", discount, "\n\t     Discounted fare is", new_fare)
+    else:
+	    print("Sorry,", name," you are not eligible for student discount.")
+    ''')
+
