f612241b9ed7c5a96455035b44caaae4ea9aaa91
dynneal <dynn5723@gmail.com>
2025-12-04 23:46:44 +0800

    ...

--- penguin.py
+++ penguin.py
@@ -14,7 +14,7 @@ while con == True:
     if option == 'yes':
         Menu()
         print("\n")
-        opt = input("Enter a number of your chosen topic (1-6): ")
+        opt = input("Enter a number of your chosen topic (1-7): ")
         os.system('cls')
         if opt == '1':
             print(f"Hi, {name}! Welcome to Printing in Python.\n")
@@ -29,6 +29,7 @@ while con == True:
                 if men == 'a':
                     printing_opt()
                     print("\n~ Simple Printing ~")
+                    print("\nIn this example, we will print a message to the user. To print a string of text, just enclose it in single or double quotes within the print() function.\nThis is an example of simple printing in Python: \n")
                     print_output()
                     print_input()
                     print("\n")
@@ -61,11 +62,11 @@ while con == True:
                     print_menu()
                 elif men == 'e':
                     sub_menu = False
-                    print(f"Hi, {name}! Welcome to Printing in Python.\n")
+                    print(f"Hi, {name}! Welcome to the program.\n")
                 else:
                     print("Invalid input. Please try again")
                     #continue to sub-menu
-                    print(f"Hi, {name}! Welcome to Printing in Python.\n")
+                    print(f"Hi, {name}! Welcome to the program.\n")
                     print_menu()
             continue
         elif opt == '2':
@@ -75,21 +76,25 @@ while con == True:
             #repeating sub-menu for conditional statements topic
             cond_sub_menu = True
             while cond_sub_menu == True:
-                answer = input("Select a letter of your choice (a-e): ").lower()
+                answer = input("\nSelect a letter of your choice (a-e): ").lower()
                 os.system('cls')
                 if answer == 'a':
-                    conditional_statements()
+                    print("~ If Statements ~")
                     if_statements()
                     os.system('pause')
                     ex_trial_if()
-                    run_ex_if = input("Do you want to try the example? (yes/no): ").lower()
+                    run_ex_if = input("*Do you want to try the example? (yes/no): ").lower()
                     print("\n")
                     if run_ex_if == 'yes':
                         run_ex()
+                        os.system('pause')
                         print("\n")
-                    else:
+                    elif run_ex_if == 'no':
                         print("You chose not to run the example.")
-                    os.system('pause')
+                        os.system('pause')
+                    else:
+                        print("Invalid input. Please try again.")
+                        continue
                     #continue to sub-menu
                     os.system('cls')
                     print(f"Hi, {name}! Welcome to Conditional Statements in Python.\n")
@@ -97,31 +102,69 @@ while con == True:
                 elif answer == 'b':
                     if_else_statements()
                     os.system('pause')
-                    ex_trial_if_else()
-                    run_ex_if_else = input("Do you want to try the example? (yes/no): ").lower()
+                    if_else_ex()
+                    run_ex_if_else = input("*Do you want to try the example? (yes/no): ").lower()
                     print("\n")
                     if run_ex_if_else == 'yes':
-                        run_ex_if_else()
+                        if_else_run()
+                        os.system('pause')
                         print("\n")
-                    else:
+                    elif run_ex_if_else == 'no':
                         print("You chose not to run the example.")
-                    os.system('pause')
+                        os.system('pause')
+                    else:
+                        print("Invalid input. Please try again.")
                     #continue to sub-menu
                     os.system('cls')
-                    print(f"Hi, {name}! Welcome to Conditional Statements in Python.\n")
+                    print(f"Hi, {name}! Welcome to the program.\n")
                     conditional_menu()
                 elif answer == 'c':
-                    pass
+                    print("~ If-Elif -Else Statements ~")
+                    if_elif_else_statement()
+                    os.system('pause')
+                    if_elif_else_ex()
+                    run = input("*Do you want to try the example? (yes/no): ")
+                    print("\n")
+                    if run == 'yes':
+                        if_elif_else_run()
+                        os.system('pause')
+                        print("\n")
+                    elif run == 'no':
+                        print("You chose not to run the example.")
+                        os.system('pause')
+                    else:
+                        print("Invalid input. Please try again.")
+                    #continue to sub-menu
+                    os.system('cls')
+                    print(f"Hi, {name}! Welcome to the program.\n")
+                    conditional_menu()
                 elif answer == 'd':
-                    pass    
+                    print("~ Nested If Statements ~")
+                    nest_statements()
+                    os.system('pause')
+                    nest_example()
+                    run_nest = input("*Do you want to try the example? (yes/no): ")
+                    print("\n")
+                    if run_nest == 'yes':
+                        nest_run()
+                        os.system('pause')
+                        print("\n")
+                    elif run_nest == 'no':
+                        print("You chose not to run the example.")
+                        os.system('pause')
+                    else:
+                        print("Invalid input. Please try again.")
+                    #continue to sub-menu
+                    os.system('cls')
+                    print(f"Hi, {name}! Welcome to the program.\n")
+                    conditional_menu()
                 elif answer == 'e':
                     cond_sub_menu = False
-                    print(f"Hi, {name}! Welcome to Conditional Statements in Python.\n")
-                    Menu()
+                    print(f"Hi, {name}! Welcome to the program.\n")
                 else:
                     print("Invalid input. Please try again")
                     #continue to sub-menu
-                    print(f"Hi, {name}! Welcome to Conditional Statements in Python.\n")
+                    print(f"Hi, {name}! Welcome to the program.\n")
                     conditional_menu()
                     continue
         elif opt == '3':
@@ -131,6 +174,8 @@ while con == True:
         elif opt == '5':
             pass
         elif opt == '6':
+            pass
+        elif opt == '7':
             print(f"Thank you for using the program, {name}! Goodbye!")
             break
         else:
